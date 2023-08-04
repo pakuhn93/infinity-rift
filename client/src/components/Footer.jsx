d24d537f69dcb52e2b0727b0063e02f5981ac9f7
Alfonso Torres <Alfonsotorres758@gmail.com>
2023-08-04 14:35:58 -0700

    nav,homepage

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,25 @@
-export default function Footer(){
-    return (
-        <div>
-            <button className="btn-footer" id='btn-about'>About</button>
-            <button className="btn-footer" id='btn-signup'>Sign up for Updates</button>
-            <button className="btn-footer" id='btn-suggestions'>Suggestions</button>
-            <button className="btn-footer" id='btn-github'>GitHub</button>
-        </div>
-    );
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <button className="btn-footer" id="btn-about">
+          About
+        </button>
+        <button className="btn-footer" id="btn-signup">
+          Sign up for Updates
+        </button>
+        <button className="btn-footer" id="btn-suggestions">
+          Suggestions
+        </button>
+        <button className="btn-footer" id="btn-github">
+          GitHub
+        </button>
+      </div>
+      <p className="footer-text">
+        © {new Date().getFullYear()} Card Battle Game. All rights reserved.
+      </p>
+    </footer>
+  );
 }
