acc385653d33c0c51ba79bbc34bd99b9120ff39a
Alfonso Torres <Alfonsotorres758@gmail.com>
2023-08-08 11:48:05 -0700

    modal

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
 import "./NavBar.css";
 
-
 export default function NavBar() {
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-logo">
-        <img src="./public/INFINITY RIFT 2.png" alt="Infinity Rift Logo" />
-        <span className="logo-text">
-          {" "}
-          <h2>INFINITY RIFT</h2> <h6>DECK DUEL DOMINATE </h6>
-        </span>
+        <img src="/logo.png" alt="Infinity Rift Logo" />
+        <span className="logo-text">Infinity Rift</span>
       </div>
       <div className="nav-links">
         <button className="btn-nav" id="btn-home">
@@ -22,10 +29,25 @@ export default function NavBar() {
         <button className="btn-nav" id="btn-login">
           Login/Signup
         </button>
-        <button className="btn-nav" id="btn-rules">
+        <button className="btn-nav" id="btn-rules" onClick={openModal}>
           Rules
         </button>
       </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <p>
+              Some text in the Modal the rule of the game the rule of the game
+              the rule of the game the rule of the game the rue of the game the
+              rule of the game the rule of the game the rule of the game the
+              rule of the game the rule of the game the rule of rthe game..
+            </p>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
